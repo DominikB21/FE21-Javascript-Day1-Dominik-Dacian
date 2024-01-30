3d989132ea7417a3d9e3067d3388f5ef13316655
Dominik Burycz <dominik.burycz@gmail.com>
2024-01-30 13:43:51 +0100

    added comments in advanced

--- Dominik/Challange/script.js
+++ Dominik/Challange/script.js
@@ -1,5 +1,5 @@
 let sentence = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up"
 
-let newSentence = new Array();
-newSentence = sentence.split('$');
-console.log(newSentence.join(" "));
+let newSentence = new Array(); //creates a new array
+newSentence = sentence.split('$'); //splits the string at ever "$" and outputs an array
+console.log(newSentence.join(" ")); //joins the strings within the array with a space inbetween
